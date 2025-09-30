0dff8a79b1e1917a7f6ce47470579bb95142d6bf
moussakbq <moussadou128@gmail.com>
2025-09-30 16:19:35 +0200

    leaks & action

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask!"
+    SECRET_KEY="odc54d76!54$"
+    return SECRET_KEY
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
